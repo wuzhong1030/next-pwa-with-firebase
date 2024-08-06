d05e4d380f2a1e7c1164b1de9677af70742bcdcb
stone.wu <stone.wu@webeye.com>
2024-08-06 20:55:17 +0800

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyiEMcSpG-JjKtOY8Wjo7FYwcBVY5BTg8",
-  authDomain: "web-push-demo-5f2bd.firebaseapp.com",
-  projectId: "web-push-demo-5f2bd",
-  storageBucket: "web-push-demo-5f2bd.appspot.com",
-  messagingSenderId: "69996489641",
-  appId: "1:69996489641:web:532dcafda552f2a47bb2b8",
-  measurementId: "G-06EEK4MX1R",
+  apiKey: "AIzaSyAZ5O1TyMSp_G7Aiqa_x8LXUBB7a9r3Ad8",
+  // authDomain: "next-pwa-with-firebase.firebaseapp.com",
+  projectId: "next-pwa-with-firebase",
+  // storageBucket: "next-pwa-with-firebase.appspot.com",
+  messagingSenderId: "83448491839",
+  appId: "1:83448491839:web:f9e912c5267a200ea0ad8a",
+  measurementId: "G-NNGVWDYMDB",
 };
 
 // Initialize Firebase
@@ -21,23 +21,33 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function (payload) {
-  const promiseChain = clients
-    .matchAll({
-      type: "window",
-      includeUncontrolled: true,
-    })
-    .then((windowClients) => {
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        windowClient.postMessage(payload);
-      }
-    })
-    .then(() => {
-      return self.registration.showNotification("my notification title");
-    });
-  return promiseChain;
+messaging.onBackgroundMessage((payload) => {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "./icons/icon-48x48.png",
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+// messaging.setBackgroundMessageHandler(function (payload) {
+//   const promiseChain = clients
+//     .matchAll({
+//       type: "window",
+//       includeUncontrolled: true,
+//     })
+//     .then((windowClients) => {
+//       for (let i = 0; i < windowClients.length; i++) {
+//         const windowClient = windowClients[i];
+//         windowClient.postMessage(payload);
+//       }
+//     })
+//     .then(() => {
+//       return self.registration.showNotification("my notification title");
+//     });
+//   return promiseChain;
+// });
 
 self.addEventListener("notificationclick", function (event) {
   // do what you want
