d3a56067e8f348b8dec7e352d448ba9d8ebf8ab2
stone.wu <stone.wu@webeye.com>
2024-08-07 11:28:22 +0800

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,8 +20,17 @@ export const metadata = {
     capable: true,
     statusBarStyle: "default",
     title: siteConfig.name,
-    startupImage: SplashScreens,
+    startupImage: ["/icons/icon-96x96.png"],
   },
+  referrer: "origin-when-cross-origin",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  shortcut: ["/icons/icon-48x48.png"],
+  apple: [{ url: "/icons/icon-144x144.png" }, { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" }],
+  manifest: '/manifest.json',
 };
 
 export const viewport = {
