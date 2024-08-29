c745a8acebbec4353cd4005ab7eca649d33a9fbe
stone.wu <stone.wu@webeye.com>
2024-08-29 10:25:07 +0800

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@
  * @Author: stone.wu stone.wu@webeye.com
  * @Date: 2024-08-07 11:43:38
  * @LastEditors: stone.wu stone.wu@webeye.com
- * @LastEditTime: 2024-08-29 10:22:10
+ * @LastEditTime: 2024-08-29 10:24:43
  * @FilePath: /next-pwa-with-firebase/public/firebase-messaging-sw.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,15 +29,15 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Received background message ", payload);
-  const notificationTitle = payload.notification.title + "111";
-  const notificationOptions = {
-    body: payload.notification.body + "222222",
-    icon: "./icons/icon-48x48.png",
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("[firebase-messaging-sw.js] Received background message ", payload);
+//   const notificationTitle = payload.notification.title + "111";
+//   const notificationOptions = {
+//     body: payload.notification.body + "222222",
+//     icon: "./icons/icon-48x48.png",
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 messaging.setBackgroundMessageHandler(function (payload) {
   const promiseChain = clients
