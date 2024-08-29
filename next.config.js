7649bb07c2973d984de5a3a6feabf6aeff7d48b5
stone.wu <stone.wu@webeye.com>
2024-08-29 09:31:12 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @Author: stone.wu stone.wu@webeye.com
  * @Date: 2024-08-29 09:23:27
  * @LastEditors: stone.wu stone.wu@webeye.com
- * @LastEditTime: 2024-08-29 09:26:19
+ * @LastEditTime: 2024-08-29 09:30:27
  * @FilePath: /next-pwa-with-firebase/next.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ const nextConfig = {
 // Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
   dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  disable: process.env.NODE_ENV !== "development", // Disable PWA in development mode
   // runtimeCaching,
   exclude: [/app-build-manifest.json/],
   // buildExcludes: [
