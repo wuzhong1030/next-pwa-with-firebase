3f5b61ac85203dfb3c41b44ee6d8e4c4c113ee5c
stone.wu <stone.wu@webeye.com>
2024-08-29 10:12:13 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -121,7 +121,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between rounded-lg gap-4">
-      <h1>web-push-with-firebase demo4</h1>
+      <h1>web-push-with-firebase demo5</h1>
       <FcmTokenComp />
       <h5>msg: {msg}</h5>
       <InstallationPrompt />
