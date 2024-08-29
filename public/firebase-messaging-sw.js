46c2b95b925d05a7cc0ffc451ba2fa5450638179
stone.wu <stone.wu@webeye.com>
2024-08-29 10:00:46 +0800

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@
  * @Author: stone.wu stone.wu@webeye.com
  * @Date: 2024-08-07 11:43:38
  * @LastEditors: stone.wu stone.wu@webeye.com
- * @LastEditTime: 2024-08-29 09:56:34
+ * @LastEditTime: 2024-08-29 10:00:40
  * @FilePath: /next-pwa-with-firebase/public/firebase-messaging-sw.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 // const messaging = firebase.messaging();
 
